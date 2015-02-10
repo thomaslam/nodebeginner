8f959f2fba495f8c33840fc473fb973991c912d5
thomaslam <tuananhlam95@gmail.com>
2015-02-10 00:39:22 -0500

    add /show request controller

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,41 @@
-var exec = require("child_process").exec;
+// var exec = require("child_process").exec;
+var querystring = require("querystring"),
+	fs = require("fs");
 
-function start(response) {
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
-	exec("ls -lah", function (error, stdout, stderr) { 
-		response.writeHead(200, {"Content-Type": "text/plain"}); 
-		response.write(stdout);
-		response.end();
-	});
-}
+	var body = '<html>'+
+	 '<head>'+
+	 '<meta http-equiv="Content-Type" content="text/html; '+
+	 'charset=UTF-8" />'+
+	 '</head>'+
+	 '<body>'+
+	 '<form action="/upload" method="post">'+
+	 '<textarea name="text" rows="20" cols="60"></textarea>'+
+	 '<input type="submit" value="Submit text" />'+
+	 '</form>'+
+	 '</body>'+
+	'</html>';
 
-function upload(response) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"}); 
-	response.write("Hello Upload");
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
 	response.end();
 }
 
+function upload(response, postData) {
+	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("You've sent: " + querystring.parse(postData).text);
+	response.end();
+}
+
+function show(response) {
+	console.log("Request handler 'show' was called.");
+	response.writeHead(200, {"Content-Type": "image/png"});
+	fs.createReadStream("/tmp/cat.png").pipe(response);
+}
+
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
